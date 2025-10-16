9acab65197ae11758c9e71572c21176647debd6b
k-metra <metrakurt@gmail.com>
2025-10-16 19:31:53 +0800

    initialize account

---
+++ src/classes/Account.py
@@ -0,0 +1,8 @@
+class Account():
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def has_permission(self, permission):
+        return (permission in self.role.permissions)
