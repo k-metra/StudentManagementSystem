4daaec0b3297fa2a8bf92717cdaafe46611021c2
k-metra <metrakurt@gmail.com>
2025-10-16 19:46:31 +0800

    initiate permissions

---
+++ enums/permissions.py
@@ -0,0 +1,10 @@
+from enum import IntFlag, auto
+
+class Permissions(IntFlag):
+    ADD_STUDENT = auto()
+    DELETE_STUDENT = auto()
+    EDIT_STUDENT = auto()
+    CREATE_ACCOUNT = auto()
+    DELETE_ACCOUNT = auto()
+    EDIT_ACCOUNT = auto()
+    VIEW_REPORTS = auto()
