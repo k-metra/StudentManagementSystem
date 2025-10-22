f28d5043cf94eca03d66debaab0766b001b1d292
k-metra <metrakurt@gmail.com>
2025-10-22 22:35:17 +0800

    refactor: remove unused load_accounts function from login.py

--- src/utils/login.py
+++ src/utils/login.py
@@ -3,11 +3,6 @@ import time
 from .file import load_file
 from .misc import clear_input_buffer
 
-def load_accounts(file_name):
-    return load_file(file_name, key="accounts")
-
-
-
 def prompt_login():
     # Small delay and clear input buffer to prevent keyboard event interference
     time.sleep(0.1)
