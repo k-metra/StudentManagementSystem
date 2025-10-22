fe2d0906b6e280fc5c8dc8aae9f11fef4a22a111
k-metra <metrakurt@gmail.com>
2025-10-22 21:46:45 +0800

    fix(login)!: fix username input being skipped over after selecting login option
    The issue is fixed by clearing the input buffer, clearing any keystrokes prior to the input. And also a split second delay

--- src/utils/login.py
+++ src/utils/login.py
@@ -1,9 +1,26 @@
+import sys
+import time
 from .file import load_file
 
 def load_accounts(file_name):
     return load_file(file_name, key="accounts")
 
+def clear_input_buffer():
+    """Clear any remaining input in the buffer to prevent interference from keyboard events"""
+    try:
+        import msvcrt
+        while msvcrt.kbhit():
+            msvcrt.getch()
+    except ImportError:
+        # For non-Windows systems
+        import termios, tty
+        sys.stdin.flush()
+
 def prompt_login():
+    # Small delay and clear input buffer to prevent keyboard event interference
+    time.sleep(0.1)
+    clear_input_buffer()
+    
     username = input("Enter username: ")
     password = input("Enter password: ")
     return username, password
