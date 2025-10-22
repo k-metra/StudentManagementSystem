aaf561ddbfecb8e499c267e8a8a02a3031df1b8e
k-metra <metrakurt@gmail.com>
2025-10-22 22:01:50 +0800

    migrate clear_input_buffer function to misc.py

--- src/utils/login.py
+++ src/utils/login.py
@@ -1,20 +1,12 @@
 import sys
 import time
 from .file import load_file
+from .misc import clear_input_buffer
 
 def load_accounts(file_name):
     return load_file(file_name, key="accounts")
 
-def clear_input_buffer():
-    """Clear any remaining input in the buffer to prevent interference from keyboard events"""
-    try:
-        import msvcrt
-        while msvcrt.kbhit():
-            msvcrt.getch()
-    except ImportError:
-        # For non-Windows systems
-        import termios, tty
-        sys.stdin.flush()
+
 
 def prompt_login():
     # Small delay and clear input buffer to prevent keyboard event interference
