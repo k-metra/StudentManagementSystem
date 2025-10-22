53ab16be2fa2d4331b7c9442cc03713d1b18000a
k-metra <metrakurt@gmail.com>
2025-10-22 21:25:49 +0800

    extract file-loading responsibility into its own file instead of combining it with the login class

--- src/utils/login.py
+++ src/utils/login.py
@@ -1,25 +1,11 @@
-import json
-
-import json
+from .file import load_file
 
 def load_accounts(file_name):
-    with open(file_name, 'r') as f:
-        data = json.load(f)
-
-    return data.get('accounts', [])
-
-def access_file(file_name):
-    accounts = load_accounts(file_name)
-    print(accounts) 
-    if accounts:
-        first = accounts[0]
-        print(f"name: {first['username']}, password: {first['password']}")
+    return load_file(file_name, key="accounts")
 
 def prompt_login():
-    print("Enter your username: ", end="", flush=True)
-    username = input()
-    print("Enter your password: ", end="", flush=True)
-    password = input()
+    username = input("Enter username: ")
+    password = input("Enter password: ")
     return username, password
 
 
