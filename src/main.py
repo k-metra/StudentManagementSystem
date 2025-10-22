21149ee15dac637aa78616a4faa2ec5c5adb0134
k-metra <metrakurt@gmail.com>
2025-10-22 21:26:19 +0800

    refactor(main): streamline account loading by moving it to a dedicated function

--- src/main.py
+++ src/main.py
@@ -4,14 +4,17 @@ from utils.clear_console import clear_console
 from classes.UserChoiceManager import UserChoiceManager
 from utils.options import options
 from termcolor import colored
-from utils.login import access_file, prompt_login, load_accounts
-
+from utils.login import prompt_login
+from utils.file import load_file
 
 def main():
 
+    # Fetch accounts on startup only once after startup for performance
+    accounts = load_accounts()
+
     user_options = options("Login", "Exit")
     UCM = UserChoiceManager(user_options, prompt=colored("<== Student Records Management System ==>\n", "cyan", attrs=["bold"]))
-    open_file = access_file("src/data/accounts.json")
+
     option = UCM.get_user_choice()
 
     if option.index == 1:
@@ -22,7 +25,6 @@ def main():
     elif option.index == 0:
         clear_console()
         print(colored("== Login Page ==\n", "cyan", attrs=["bold"]))
-        accounts = load_accounts("src/data/accounts.json")
         username, password = prompt_login()
 
         for acc in accounts:
@@ -32,4 +34,8 @@ def main():
         
         print(colored("\nInvalid username or password. Please try again.", "red"))
         return
+    
+def load_accounts():
+    return load_file("src/data/accounts.json", key="accounts")
+
 main()
