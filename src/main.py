42efbdafb014cf3b4552a9fe1be3a1339c1d0260
stimms1 <bluearinsol0718@gmail.com>
2025-10-22 18:21:49 +0800

    Started developing the login functionality

--- src/main.py
+++ src/main.py
@@ -4,18 +4,23 @@ from utils.clear_console import clear_console
 from classes.UserChoiceManager import UserChoiceManager
 from utils.options import options
 from termcolor import colored
+from utils.login import access_file
+
 
 def main():
     user_options = options("Login", "Exit")
     UCM = UserChoiceManager(user_options, prompt=colored("<== Student Records Management System ==>\n", "cyan", attrs=["bold"]))
-
+    open_file = access_file("src/data/accounts.json")
     choice = UCM.get_user_choice()
 
     if choice == 1:
         print(colored("Exiting the program. Goodbye!", "yellow"))
+        
         return
-    elif choice == 2:
+    elif choice == 0:
+        print(open_file)
+        
         print(colored("Login functionality is not yet implemented.", "red"))
+        
         return
-
 main()
