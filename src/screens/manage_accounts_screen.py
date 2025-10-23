f100c92a6a48b6b3c232f5f1e12666c67c161b7e
k-metra <metrakurt@gmail.com>
2025-10-23 17:29:49 +0800

    feat: enhance account management screen with improved user prompts and input handling

--- src/screens/manage_accounts_screen.py
+++ src/screens/manage_accounts_screen.py
@@ -7,6 +7,7 @@ from classes.UserChoiceManager import UserChoiceManager
 from utils.misc import enter_to_continue
 from utils.misc import clear_input_buffer
 from termcolor import colored
+import pwinput
 
 import time
 
@@ -30,89 +31,113 @@ def manage_accounts_screen(current_account: Account, choice_manager: UserChoiceM
 
     while True:
         clear_console()
-        print(header)
-
         line = "Account ID".ljust(12) + "Username".ljust(20) + "Role".ljust(15)
-        print(colored(line, "white", attrs=["bold"]))
-        print("-" * 47)
+
+        prompt = colored(line, "white", attrs=["bold"])
+        prompt += "\n" + ("-" * (12 + 20 + 15)) + "\n"
 
         for user_account in user_accounts:
             user_id = str(user_account.get("id"))
             username = user_account.get("username")
             role = user_account.get("data").get("role")
 
-            print(user_id.ljust(12), username.ljust(20), role.ljust(15))
+            account_line = user_id.ljust(12) + username.ljust(20) + role.ljust(15)
+
+            prompt += account_line + "\n"
+
         
-        user_id = input("\nEnter the Account ID to manage (0 to exit): ")
+        choice_manager.set_prompt(new_prompt=prompt)
+        choice_manager.set_options([
+            "Create New Account",
+            "Manage Existing Account",
+            "Back to Main Menu"
+        ])
 
-        try:
-            user_id = int(user_id)
-        except ValueError:
-            print(colored("Invalid input. Please enter a valid Account ID.", "red"))
-            enter_to_continue()
-            continue
+        choice = choice_manager.get_user_choice()
 
-        if user_id == 0:
-            return
-        else:
-            if user_id < 1 or user_id > len(user_accounts):
-                print(colored("Invalid Account ID. Please try again.", "red"))
-                enter_to_continue()
-                continue
+        match choice.label():
+            case "Back to Main Menu":
+                return
+            case "Create New Account":
+                username = input("Enter new username: ")
+                password = pwinput.pwinput("Enter new password: ")
+                role = input("Enter role for the new account: ")
+                result = controller.create_account(username=username, password=password, role_name=role)
 
-            selected_account = user_accounts[user_id - 1]
+                if result.get("status"):
+                    print(colored(result.get("message"), "green"))
+                    enter_to_continue()
+                    continue 
+            case "Manage Existing Account":
+                user_id = input("\nEnter the Account ID to manage (0 to cancel): ")
 
-            while True:
-                user_choice_manager = UserChoiceManager()
+                try:
+                    user_id = int(user_id)
+                except ValueError:
+                    print(colored("Invalid input. Please enter a valid Account ID.", "red"))
+                    enter_to_continue()
+                    continue
 
-                prompts = [
-                    colored(f"<== Managing Account: {selected_account.get('username')} ==>", "cyan", attrs=["bold"]),
-                    colored(f"Account ID:", "cyan", attrs=["bold"]) + colored(f" {selected_account.get('id')}", "white", attrs=[]),
-                    colored(f"Username:", "cyan", attrs=["bold"]) + colored(f" {selected_account.get('username')}", "white", attrs=[]),
+                if user_id == 0:
+                    continue
+                else:
+                    if user_id < 1 or user_id > len(user_accounts):
+                        print(colored("Invalid Account ID. Please try again.", "red"))
+                        enter_to_continue()
+                        continue
+
+                selected_account = user_accounts[user_id - 1]
+
+                while True:
+                    user_choice_manager = UserChoiceManager()
+
+                    prompts = [
+                        colored(f"<== Managing Account: {selected_account.get('username')} ==>", "cyan", attrs=["bold"]),
+                        colored(f"Account ID:", "cyan", attrs=["bold"]) + colored(f" {selected_account.get('id')}", "white", attrs=[]),
+                            colored(f"Username:", "cyan", attrs=["bold"]) + colored(f" {selected_account.get('username')}", "white", attrs=[]),
                     colored(f"Role:", "cyan", attrs=["bold"]) + colored(f" {selected_account.get('data').get('role')}", "white", attrs=[]),
                 ]
                 
-                new_prompt = "\n".join(prompts)
-                user_choice_manager.set_prompt(new_prompt)
-                user_choice_manager.set_options([
-                    "Change Password",
-                    "Change Role",
-                    "Delete Account",
-                    "Back to Account List"
-                ])
+                    new_prompt = "\n".join(prompts)
+                    user_choice_manager.set_prompt(new_prompt)
+                    user_choice_manager.set_options([
+                        "Change Password",
+                        "Change Role",
+                        "Delete Account",
+                        "Back to Account List"
+                    ])
 
-                choice = user_choice_manager.get_user_choice()
-                clear_input_buffer()
-                time.sleep(.1)
+                    choice = user_choice_manager.get_user_choice()
 
-                match choice.label():
-                    case "Change Password":
-                        new_password = input("Enter new password: ")
-                        result = controller.update_account(username=selected_account.get("username"), password=new_password)
-                        if result.get("status"):
-                            print(colored(result.get("message"), "green"))
-                        else:
-                            print(colored(result.get("error"), "red"))
-                        enter_to_continue()
+                    match choice.label():
+                        case "Change Password":
+                            new_password = input("Enter new password: ")
+                            result = controller.update_account(username=selected_account.get("username"), password=new_password)
 
-                    case "Change Role":
-                        new_role = input("Enter new role: ")
-                        result = controller.update_account(username=selected_account.get("username"), role_name=new_role)
-                        if result.get("status"):
-                            print(colored(result.get("message"), "green"))
-                        else:
-                            print(colored(result.get("error"), "red"))
-                        enter_to_continue()
-
-                    case "Delete Account":
-                        confirm = input("Are you sure you want to delete this account? (y/n): ")
-                        if confirm.lower() == "y":
-                            result = controller.delete_account(username=selected_account.get("username"))
                             if result.get("status"):
                                 print(colored(result.get("message"), "green"))
                             else:
                                 print(colored(result.get("error"), "red"))
-                        enter_to_continue()
+                            enter_to_continue()
 
-                    case "Back to Account List":
-                        break
+                        case "Change Role":
+                            new_role = input("Enter new role: ")
+                            result = controller.update_account(username=selected_account.get("username"), role_name=new_role)
+                            if result.get("status"):
+                                print(colored(result.get("message"), "green"))
+                            else:
+                                print(colored(result.get("error"), "red"))
+                            enter_to_continue()
+
+                        case "Delete Account":
+                            confirm = input("Are you sure you want to delete this account? (y/n): ")
+                            if confirm.lower() == "y":
+                                result = controller.delete_account(username=selected_account.get("username"))
+                                if result.get("status"):
+                                    print(colored(result.get("message"), "green"))
+                                else:
+                                    print(colored(result.get("error"), "red"))
+                                enter_to_continue()
+
+                        case "Back to Account List":
+                            break
