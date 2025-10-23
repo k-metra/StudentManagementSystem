9703dda482cb21ff876ac8a7ea58f3c5af25fc08
k-metra <metrakurt@gmail.com>
2025-10-23 09:39:46 +0800

    refactor: enhance Role class initialization and subclass registration

--- src/classes/Role.py
+++ src/classes/Role.py
@@ -1,7 +1,15 @@
 class Role():
-    def __init__(self, name, permissions):
-        self.name = name
-        self.permissions = permissions
+    registry = {} # A registry to hold all role subclasses
+    
+    def __init_subclass__(cls, **kwargs):
+        # Automatically register role subclasses by their names into the registry
+        # for easier lookup later.
+        super().__init_subclass__(**kwargs)
+        Role.registry[cls.__name__] = cls
+
+    def __init__(self, name=None, permissions=None):
+        self.name = name or self.__class__.__name__
+        self.permissions = permissions or []
 
     def has_permission(self, permission):
         return permission in self.permissions
