bd99287c49bf9451e04feba9b3e4e3bb5dce01dc
k-metra <metrakurt@gmail.com>
2025-10-23 09:40:44 +0800

    fix: remove missing permissions enum

--- src/roles/Staff.py
+++ src/roles/Staff.py
@@ -4,7 +4,6 @@ from enums.permissions import Permissions
 class Staff(Role):
     def __init__(self):
         super().__init__(permissions=[
-            Permissions.VIEW_STUDENT,
             Permissions.ADD_STUDENT,
             Permissions.EDIT_STUDENT,
         ])
