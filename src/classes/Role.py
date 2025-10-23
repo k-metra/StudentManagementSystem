4e67478a4909b9d66758718ce54ebfa76f5881dc
k-metra <metrakurt@gmail.com>
2025-10-23 08:33:27 +0800

    feat: enhance Role class with permission checking method

--- src/classes/Role.py
+++ src/classes/Role.py
@@ -2,3 +2,6 @@ class Role():
     def __init__(self, name, permissions):
         self.name = name
         self.permissions = permissions
+
+    def has_permission(self, permission):
+        return permission in self.permissions
