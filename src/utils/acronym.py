642ebfa1eee3c713138f529e2e28fa1c363a181a
k-metra <metrakurt@gmail.com>
2025-10-30 12:22:13 +0800

    implement utility class for acronyms

---
+++ src/utils/acronym.py
@@ -0,0 +1,27 @@
+def acronymize(phrase: str) -> str:
+    """Generate an acronym from a given phrase.
+    Good for college departments, e.g. CCS -> College of Computer Studies
+    """
+
+    non_acronym_words = ["of", "and", "the", "for", "in", "on", "at"]
+
+    words = phrase.split()
+    acronym = "".join(word[0].upper() for word in words if word not in non_acronym_words)
+
+    return acronym 
+
+def expand_acronym(acronym: str, mapping: dict | None = None) -> str:
+
+    mapping = mapping or {
+        "CCS":      "College of Computer Studies",
+        "CAS":      "College of Arts and Sciences",
+        "COENG":    "College of Engineering",
+        "CBM":      "College of Business Management",
+        "COL":      "College of Law",
+        "CCJ":      "College of Criminal Justice",
+        "CTHM":     "College of Tourism and Hospitality Management",
+        "COED":     "College of Education",
+        "CAS":      "College of Arts and Sciences",
+    }
+
+    return mapping.get(acronym.upper(), acronym)
