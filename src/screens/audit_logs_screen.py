0997b59e5f934629d5ce39b02975bd6d569fd140
k-metra <metrakurt@gmail.com>
2025-11-03 22:26:10 +0800

    feat(audit_logs): create initial audit logs screen function

---
+++ src/screens/audit_logs_screen.py
@@ -0,0 +1,7 @@
+
+
+from classes.Account import Account
+from classes.AccountManager import AccountManager
+
+def audit_logs_screen(current_account: Account, account_manager: AccountManager) -> None:
+    pass
