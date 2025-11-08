6177c2b613d6d8eacb540f55758da6ecfb981728
k-metra <metrakurt@gmail.com>
2025-11-09 00:19:19 +0800

    fix(format_phone): refine regex logic and phone formatting

--- src/utils/regex.py
+++ src/utils/regex.py
@@ -7,9 +7,19 @@ def validate_phone(num) -> bool:
     return bool(re.match(pattern, num))
 
 def format_phone(num) -> str:
+
+    num = re.sub(r"\D", "", num)
+
+    if num.startswith("0"):
+        num = "+63" + num[1:]
+    elif not num.startswith("+63"):
+        num = "+63" + num
+
     pattern = r"^(?:\+63)(\d{3})(\d{3})(\d{4})$"
     match = re.match(pattern, num)
     if match:
+        print("Matched, returning " + f"+63 {match.group(1)} {match.group(2)} {match.group(3)}")
         return f"+63 {match.group(1)} {match.group(2)} {match.group(3)}"
     
+    print("Didn't match")
     return num
