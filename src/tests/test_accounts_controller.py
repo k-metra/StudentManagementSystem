3029e142bb59d767a5a214201a02a9378fbc9bfa
k-metra <metrakurt@gmail.com>
2025-11-08 14:05:14 +0800

    test: add unit tests for accounts controller

---
+++ src/tests/test_accounts_controller.py
@@ -0,0 +1,45 @@
+import json
+import tempfile
+from controllers.ManageAccountsController import ManageAccountsController
+from classes.Account import Account
+from classes.Role import Role 
+from enums.permissions import Permissions
+
+def test_create_account_success(monkeypatch):
+    role = Role(name="User", permissions=[Permissions.CREATE_ACCOUNT])
+    admin = Account("admin", "pass", role)
+    controller = ManageAccountsController(admin)
+
+    controller.accounts = {}
+    controller.account_manager._role_registry = {"Admin": Role}
+    
+    monkeypatch.setattr(controller, "save_accounts", lambda: None)
+
+    result = controller.create_account("newuser", "newpass", "Admin")
+    assert result["status"] is True 
+
+def test_create_account_existing_user(monkeypatch):
+    role = Role("Admin", permissions=[Permissions.CREATE_ACCOUNT])
+
+    admin = Account("admin", "pass", role)
+
+    controller = ManageAccountsController(admin)
+    controller.accounts = {"newuser": {"password": "123", "role": "Admin"}}
+    controller.account_manager._role_registry = {"Admin": Role}
+    monkeypatch.setattr(controller, "save_accounts", lambda: None)
+
+    result = controller.create_account("newuser", "newpass", "Admin")
+    assert result["status"] is False
+
+def test_create_account_fail(monkeypatch):
+    role = Role(name="User", permissions=[])
+    account = Account("UserAccount", "pass", role=role)
+    controller = ManageAccountsController(account)
+
+    controller.accounts = {}
+    controller.account_manager._role_registry = {"User": Role}
+
+    monkeypatch.setattr(controller, "save_accounts", lambda: None)
+
+    result = controller.create_account("newuser", "newpass", "Admin")
+    assert result["status"] is False
